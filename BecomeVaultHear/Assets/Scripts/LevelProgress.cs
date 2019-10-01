dcd053c3c3855b44cb29c28ee3ef1fbc757ecbf7
xmarshall39 <48485277+xmarshall39@users.noreply.github.com>
2019-10-01 09:15:08 -0400

    fixed untracked files

--- BecomeVaultHear/Assets/Scripts/LevelProgress.cs
+++ BecomeVaultHear/Assets/Scripts/LevelProgress.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class LevelProgress : MonoBehaviour
 {
+    public SpriteRenderer sprite;
+    public Transform playerPos, goalPos;
+    public 
+
+    //Reference some sprite on a canvas
+    //Two y positions: Start and Destination
+    //Have transform of Goal and Player at all times
+    //Clamp the distance between Player and Goal between 0 and 1 (maybe normalize the vector)
+    //Starting y is -5, end is 5
+    //Get the normalized value * 10 will give us what we want
+    //YES! We know the cane is 10 units tall
+
     // Start is called before the first frame update
     void Start()
     {
